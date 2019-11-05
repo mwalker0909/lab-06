146b56a56ac4f93e7812ce2df52cb1101cc96d61
Mason Walker <mwalker0909@gmail.com>
2019-11-05 11:36:12 -0800

    setting constructor

--- server.js
+++ server.js
@@ -1,45 +1,57 @@
 'use strict'
 
 // dependancies
-
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 
+
 // enviremont variables
-
-
 const PORT = process.env.PORT;
 
 
-
-
-
 // set up
-
 const app = express();
 app.use(cors());
 
 
 //routes
-
 app.get('/', (request,response) => {
   response.send('Home Page!');
 });
 
+app.get('/location', (request,response) => {
+  //TODO envoke function that converts search query to lat/long. 
 
-//entreis points
+});
 
+
+//entries points
 app.listen(PORT, () => console.log(`App is listening on ${PORT}`));
-
-
-
-
-
 
 
 // handlers
 // models
+
+//******LOCATION CONSTRUCTOR FUNCTION HERE*******. 
+function Location(query, data) {
+  this.search_query = query;
+  this.formatted_query = geoData.results[0].formatted_address;
+  this.latitude = geoData.results[0].geometry.location.lat;
+  this.longitude = geoData.results[0].geometry.location.lng;
+}
+
+
+
+
 //helpers
 
 
+
+//****** LOCATION HELPER FUNCTION HERE.  ********/
+
+
+// take in response data. 
+// new a location object using the data. 
+// catch any errors. 
+// 
